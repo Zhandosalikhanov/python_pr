400a4b9911fb80c99db3a510037d25c959d56735
Zhandos <z_alikhanov@kbtu.kz>
2022-03-28 23:54:44 +0600

    another night update

--- LABS/lab7/mus_player.py
+++ LABS/lab7/mus_player.py
@@ -1,12 +1,14 @@
-import pygame as pg
+import os, pygame as pg
 from pygame.mixer import music
-import os
+from datetime import datetime
 
 def turn_on(surface):
     fps = pg.time.Clock()
     for i in range(1, 48):
             screen = pg.image.load('data/frames/f (' + str(i) + ').jpg')
             surface.blit(screen, (0, 0))
+            surface.blit(name_text, pos)
+            # surface.blit(name_time, time_pos)                                
             pg.display.flip()
             fps.tick(50)
 
@@ -15,6 +17,8 @@ def turn_off(surface):
     for i in range(48, 1, -1):
             screen = pg.image.load('data/frames/f (' + str(i) + ').jpg')
             surface.blit(screen, (0, 0))
+            surface.blit(name_text, pos)
+            # surface.blit(name_time, time_pos)                                
             pg.display.flip()
             fps.tick(50)
 
@@ -39,8 +43,10 @@ def main():
     global songs
     songs = []
     for song in os.listdir('data/songs'):
-        music.load('data/songs/' + song)
         songs.append('data/songs/' + song)
+    
+    s = len(songs)
+    
     
     icon = pg.image.load('data/mus_icon.png').convert_alpha()
     disk = pg.image.load('data/disk.png').convert_alpha()
@@ -56,15 +62,37 @@ def main():
     rotate(surface, disk, (93, 150), disk_pivot, angle)
     
     cur_song = 0
+    cur_vol = 0.1
+    music.set_volume(cur_vol)
     On = False
     Paused = False
     Next = False
     Prev = False
+    V_up = False
+    V_down = False
     
     fps = pg.time.Clock()
     going = True    
+    start = 0
     
     while going:
+        
+        #global name_time, time_pos
+        # song_size = (pg.mixer.Sound(songs[cur_song % s])).get_length()
+        # duration = 'Duration: ' + str(int((music.get_pos() * 1000) % 60)) + ':' + str(abs((music.get_pos() * 1000) // 60)) + '/' + str(int(song_size // 60))
+        # time_pos = (223 - name_time.get_width() // 2, 180 - name_time.get_height() // 2)
+        # name_time = time_font.render(duration, True, 0, (225, 221, 218))
+        # time_font = pg.font.SysFont("comicsansms", 12)
+        
+        global name_text, pos    
+        name_font = pg.font.SysFont("comicsansms", 14)
+        
+        song_name = os.path.split(songs[cur_song % s])[1][:-4]
+        song_name = song_name.center(25)
+        
+        name_text = name_font.render(song_name, True, 0, (225, 221, 218))  
+        pos = (240 - name_text.get_width() // 2, 122 - name_text.get_height() // 2)
+        
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 going = False
@@ -83,8 +111,13 @@ def main():
                 Next = True
             if e.type == pg.KEYDOWN and e.key == pg.K_LEFT:
                 Prev = True
-                
+            if e.type == pg.KEYDOWN and e.key == pg.K_UP:
+                V_up = True
+            if e.type == pg.KEYDOWN and e.key == pg.K_DOWN:
+                V_down = True
+                 
         if On and not music.get_busy():
+            music.load(songs[cur_song % s])
             if Paused:
                 music.unpause()
             else:
@@ -102,17 +135,27 @@ def main():
         if Next:
             Next = False
             cur_song += 1
-            
             music.stop()
-            music.load(songs[cur_song])
+            music.load(songs[cur_song % s])
         
         if Prev:
             Prev = False
             cur_song -= 1
-            
             music.stop()
-            music.load(songs[cur_song])    
-                                
+            music.load(songs[cur_song % s])
+        
+        if V_up:
+            cur_vol += 0.1
+            music.set_volume(cur_vol)  
+            V_up = False
+        
+        if V_down:
+            cur_vol -= 0.1
+            music.set_volume(cur_vol)     
+            V_down = False       
+        
+        surface.blit(name_text, pos)                                
+        # surface.blit(name_time, time_pos)                                
         pg.display.flip()
         fps.tick(60)
             
---
+++ LABS/lab7/red_ball.py
@@ -0,0 +1,9 @@
+import pygame as pg 
+
+def main():
+    d = 1
+
+if __name__ == '__main__':
+    pg.init()
+    main()
+    pg.quit()
